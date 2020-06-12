548d87a8e1491527eb7aafd12f7e0cc0303e3c5f
angelique88 <66839037+angelique88@users.noreply.github.com>
2020-06-13 00:01:35 +0800

    Update and rename week 4.py to student-course-role.py

--- student-course-role.py
+++ student-course-role.py
@@ -4,25 +4,25 @@ import json
 conn = sqlite3.connect('rosterdb.sqlite')
 c = conn.cursor()
 
-# c.executescript('''
-# DROP TABLE IF EXISTS User;
-# DROP TABLE IF EXISTS Member;
-# DROP TABLE IF EXISTS Course;
-# CREATE TABLE User (
-#     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-#     name   TEXT UNIQUE
-# );
-# CREATE TABLE Course (
-#     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-#     title  TEXT UNIQUE
-# );
-# CREATE TABLE Member (
-#     user_id     INTEGER,
-#     course_id   INTEGER,
-#     role        INTEGER,
-#     PRIMARY KEY (user_id, course_id)
-# )
-# ''')
+c.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
+);
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
+''')
 
 fname = input("Enter file name: ")
 if len(fname) < 1:
